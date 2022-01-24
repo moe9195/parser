c586f816be31e7b8888f927b2ad69e4ee3095ffc
moe <moerahmeh95@hotmail.com>
2022-01-24 19:37:12 +0200

    Re-added theme.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './Styles/App.css';
 import React from 'react';
 import Window from './Components/Window';
 import {
+  createMuiTheme,
   ThemeProvider,
   CssBaseline
 } from '@material-ui/core';
 
 function App() {
+
+  const theme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
 
   return (
     <ThemeProvider theme={theme}>
